9777ad3a3d1ff63d6aa28b79359e201db6a2254c
FilipSkalski <fs237772@students.mimuw.edu.pl>
2014-04-05 17:09:50 +0200

    startup script

---
+++ s.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+SLURM_VER="slurm-14.03.0"
+BRANCH="ostrich_branch"
+
+OWNER=$(whoami)
+HOSTNAME=$(hostname)
+
+APP_DIR="deployment"
+LINKS_DIR="links"
+
+PROJECT_ROOT=$(pwd)
+APP_ROOT="${PROJECT_ROOT}/${APP_DIR}"
+LINKS_ROOT="${PROJECT_ROOT}/${LINKS_DIR}"
+
+#echo $HOSTNAME
+#echo $PROJECT_ROOT
+#echo $APP_ROOT
+
+if [ "$1" = "startup" ] || [ "$1" = "reset" ]; then
+
+    if [ "$1" = "startup" ] && [ -e "$SLURM_VER" ]; then
+        echo "Directory already exists:" $SLURM_VER
+        echo "Use 'reset' to force the operation"
+        exit 1
+    fi
+
+    rm -rf $LINKS_DIR
+    rm -rf $APP_DIR
+
+    mkdir -p ${LINKS_DIR}
+    mkdir -p ${APP_DIR}
+    mkdir ${APP_DIR}"/var" ${APP_DIR}"/etc"
+
+    rm -rf $SLURM_VER
+    tar -xf ${SLURM_VER}".tar.bz2"
+
+    cp -r ${BRANCH}/* ${SLURM_VER}/
+
+    cd $SLURM_VER
+    ./configure --prefix=$APP_ROOT --enable-multiple-slurmd --without-munge --with-ssl
+    make -s
+    make -s install
+    cd ..
+
+elif [ "$1" = "links" ]; then
+
+    ln -sf ${APP_ROOT}"/bin/*" -t ${LINKS_ROOT}
+    ln -sf ${APP_ROOT}"/sbin/*" -t ${LINKS_ROOT}
+    ln -sf ${APP_ROOT}"/var" ${LINKS_ROOT}"/var"
+    ln -sf ${APP_ROOT}"/etc" ${LINKS_ROOT}"/etc"
+
+elif [ "$1" = "config" ] || [ "$1" = "db_config" ]; then
+
+    if [ "$1" = "config" ]; then
+        DEST_FILE="/etc/slurm.conf"
+    else
+        DEST_FILE="/etc/slurmdbd.conf"
+    fi
+
+    if [ $# -lt 2 ]; then
+        echo "missing config filename"
+    else
+        cat $2 | sed s@"{HOSTNAME}"@$HOSTNAME@g | sed s@"{OWNER}"@$OWNER@g          \
+        | sed s@"{PROJECT_ROOT}"@$PROJECT_ROOT@g | sed s@"{APP_ROOT}"@$APP_ROOT@g   \
+        > ${APP_DIR}${DEST_FILE}
+    fi
+
+elif [ "$1" = "make" ]; then
+
+    #SCHED_DIR="/src/plugins/sched/ostrich"
+    #SCHED_DIR="/src/plugins/sched/"
+
+    #cp -r ${BRANCH}${SCHED_DIR}/* ${SLURM_VER}${SCHED_DIR}/
+
+    #SCHEDULERS=${SLURM_VER}${SCHED_DIR}
+
+    #make -s -C $SCHEDULERS
+
+    #if [ $? -eq 0 ]; then
+        #make -s -C $SCHEDULERS install > /dev/null
+        #echo "done"
+    #else
+        #echo "errors found"
+    #fi
+
+elif [ "$1" = "cleanup" ]; then
+
+    rm -rf $SLURM_VER $APP_DIR $LINKS_DIR
+    python clean.py
+
+else
+    echo "Available commands:"
+    echo "startup/reset, links, config, make, cleanup"
+fi
